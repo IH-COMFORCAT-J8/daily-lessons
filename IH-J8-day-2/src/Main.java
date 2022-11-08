372b7f5e7d563a99e68de223e3f3e75a4d442da0
Jaume Sánchez <jasato89@gmail.com>
2022-11-08 12:00:54 +0100

    Loops exercises

--- IH-J8-day-2/src/Main.java
+++ IH-J8-day-2/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -19,13 +21,13 @@ public class Main {
         // Bucle for each
         System.out.println("===Bucle for each===");
 
-        for(int n : numbers2) {
+        for (int n : numbers2) {
             System.out.println(n);
         }
 
         System.out.println("===Bucle while===");
 
-        int index = 12  ;
+        int index = 12;
 
 
         while (index < numbers2.length) {
@@ -38,10 +40,105 @@ public class Main {
             System.out.println("Hola clase");
             index++;
 
-        } while (index <10);
+        } while (index < 10);
+
+        System.out.println("===Ejercicio 1===");
+        int[] array = {1, 4, 5, 7, 8, 9};
+
+
+        for (int i : multiplyElements(array)) {
+            System.out.println(i);
+        }
+
+        array = new int[]{1, 4, 5, 7, 8, 9};
+        System.out.println("===Ejercicio 2===");
+
+        int[] resultInvertArray = invertArray(array);
+
+        System.out.println(Arrays.toString(resultInvertArray));
+
+        int newNumber = 5;
+
+
+        System.out.println("=== Ejercicio 3===");
+        int resultSumNumbers = sumAllNumbers(newNumber);
+
+        System.out.println(resultSumNumbers);
+
+        System.out.println("===Ejercicio 4===");
+
+        int[] oddAndEvenNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 10};
+
+        int[] evenNumbers = getEvenNumbers(oddAndEvenNumbers);
+
+        System.out.println(Arrays.toString(evenNumbers));
 
 
 
+    }
 
+    private static int[] getEvenNumbers(int[] array) {
+
+        int size = 0;
+
+        for (int i: array) {
+            if (i % 2 == 0) {
+                size++;
+            }
+        }
+        int[] result = new int[size];
+        int counter = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] %2 == 0) {
+                result[counter++] = array[i];
+            }
+
+        }
+        return result;
+
+    }
+
+    private static int sumAllNumbers(int number) {
+
+        int result = 0;
+        for (int i = 1; i <= number; i++) {
+            result += i;
+            //result = result + i;
+
+        }
+        return result;
+
+    }
+
+
+    public static int[] multiplyElements(int[] elements) {
+
+        int firstPosition = elements[0];
+        for (int i = 0; i < elements.length; i++) {
+
+            if (i < elements.length - 1) {
+                elements[i] = elements[i] * elements[i + 1];
+            } else {
+                elements[i] = elements[i] * firstPosition;
+            }
+
+        }
+
+        return elements;
+    }
+
+    public static int[] invertArray(int[] array) {
+
+        int[] result = new int[array.length];
+
+        int counter = 0;
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            result[counter++] = array[i];
+
+        }
+
+        return result;
     }
 }
