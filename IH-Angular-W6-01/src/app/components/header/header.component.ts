b78c81daa329d1713af09463f2a9274dd8cfd691
Jaume Sánchez <jasato89@gmail.com>
2022-12-13 09:45:33 +0100

    added 7.02

--- IH-Angular-W6-01/src/app/components/header/header.component.ts
+++ IH-Angular-W6-01/src/app/components/header/header.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  storeName: string;
+  formattedDate: string;
+  isDisabledSubmit: boolean;
+  isDisabledOther: boolean;
+  inputPlaceholder: string;
+  constructor() {
+    this.storeName = 'My store';
+    this.formattedDate = new Date().toLocaleDateString("es-ES");
+    this.isDisabledSubmit = true;
+    this.isDisabledOther = true;
+    this.inputPlaceholder = 'Put your text here';
+  }
 
   ngOnInit(): void {
+  }
+
+  onCopy() {
+    alert('Enabling buttons')
+    this.isDisabledSubmit = false;
+    this.isDisabledOther = false;
+  }
+
+  disableBtn(event: Event) {
+    console.log(event) // Print in console the event so the students can understand were to look for the data they need.
+    if ((<HTMLInputElement>event.target).name == 'submit-btn') { //target must be casted because it's a generic type so Angular doesn't recognize the name property
+      this.isDisabledSubmit = true;
+    } else {
+      this.isDisabledOther = true;
+    }
+
   }
 
 }
